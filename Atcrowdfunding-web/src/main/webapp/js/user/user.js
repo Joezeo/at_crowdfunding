f8cd34901d69cc826cd432c2fa346700c84d47bc
Joezeo <1252795700@qq.com>
2019-12-10 13:49:19 +0800

    修复修改按钮存在的bug

--- Atcrowdfunding-web/src/main/webapp/js/user/user.js
+++ Atcrowdfunding-web/src/main/webapp/js/user/user.js
@@ -41,9 +41,15 @@ function loadEditPage() {
     $("#content_div").load("edit.htm?t=" + Math.random());
 
     var id = $(this).parent().attr("userId");
-    // 把id绑定当content_div上
-    $("#content_div").data("id",id);
 
+    // 加上了 alert 就执行成功，不加就失败
+    // 这种情况一般出现在 alert() 之后的某个代码需要页面元素进入一定的状态才能使用，
+    // 加上 alert() 之后，相当于页面元素有足够的时间进入一定的状态了，如果确定你的代码没有问题，
+    // 你可以把 alert() 之后的代码放到一个 setTimeout 的函数中，也就是停一会再运行下面的代码，
+    setTimeout(doAjax,5, id);
+}
+
+function doAjax(id) {
     $.ajax({
         url: '/user/doQueryUser.do?t=' + Math.random(),
         data: {id: id},
@@ -68,4 +74,5 @@ function fillDataInEdit(user) {
     $("#edit-loginacct").val(user.loginacct);
     $("#edit-email").val(user.email);
     $("#edit-username").val(user.username);
+    $("#edit-id").val(user.id);
 }
